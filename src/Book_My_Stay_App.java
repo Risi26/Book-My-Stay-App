eb171cc85f1795fbf76fe43eb51fba304b993d81
risi26 <risinigarish777@gmail.com>
2026-03-16 14:51:05 +0530

    UC2:Basic Room Types & Static Availability

--- src/Book_My_Stay_App.java
+++ src/Book_My_Stay_App.java
@@ -13,5 +13,31 @@ public class Book_My_Stay_App {
         System.out.println("Ready for further operations.");
 
         System.out.println("-------------------------------------------");
+
+    }
+}
+
+abstract class Room {
+
+    protected int numberOfBeds;
+
+    protected int squareFeet;
+
+    protected double pricePerNight;
+
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+
+    public void displayRoomDetails() {
+        System.out.println("Room Details:");
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size (sq ft): " + squareFeet);
+        System.out.println("Price per Night: $" + pricePerNight);
+        System.out.println("--------------------------------");
     }
 }
